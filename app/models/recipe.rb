4b4a3fc12bb5ae2b0d4f86ec5d713317cdc37590
Nicollas Isabel <nicollasisabel@hotmail.com>
2016-11-28 20:32:04 -0200

    Commit without environment variables

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,13 @@
 class Recipe
 	include HTTParty
 
-	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
-	key_value = ENV['FOOD2FORK_KEY']
+	#hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+	#key_value = ENV['FOOD2FORK_KEY']
 
 	default_options.update(verify: false)
-	base_uri "http://#{hostport}/api/search"
+	base_uri "http://www.food2fork.com/api/search"
 	#base_uri "http://www.food2fork.com/api/search"
-	default_params key: key_value #7de14af89e1d9adee3c9fc61fd9108c4
+	default_params key: "7de14af89e1d9adee3c9fc61fd9108c4"
 	format :json
 
 	def self.for term
